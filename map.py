41d98d25af9e5ce5ebcfb3007b9d590efeea123f
karthegamer <ravuru2012@gmail.com>
2025-07-12 18:14:00 -0700

    Update map.py

--- map.py
+++ map.py
@@ -3,9 +3,9 @@ import geopandas as gpd
 from matplotlib import pyplot as plt
 import matplotlib.colors as mcolors
 print("DONE! ")
-data = pandas.read_csv(r'C:\Users\pries\ai_landslide_predictions.csv')
+data = pandas.read_csv(r'ai_landslide_predictions.csv')
 print("DONE! ")
-shape_data = gpd.read_file(r'C:\Users\pries\OneDrive\Desktop\Games\PythonPrograms\Face\Flood Model\WA_Soils.zip')
+shape_data = gpd.read_file(r'WA_Soils.zip')
 print("DONE! ")
 # Merge the results
 revised_map_data = shape_data.merge(data[["OBJECTID", "SOIL_LANDSLIDE_HAZARD"]], on="OBJECTID", how="left")
